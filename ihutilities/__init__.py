8afd69c76664b916eeaa6b00d5e767964e5c4ff1
Ian Hopkinson <ianhopkinson@googlemail.com>
2016-01-15 10:46:43 +0000

    Add dictionary display/processing functions from kaggle-forest-cover

--- ihutilities/__init__.py
+++ ihutilities/__init__.py
@@ -4,5 +4,5 @@
 from .db_utils import (configure_db, write_to_db, update_to_db, 
                        drop_db_tables, read_db, finalise_db)
 from .git_utils import git_uncommitted_changes, git_sha
-from .io_utils import write_dictionary
+from .io_utils import write_dictionary, pretty_print_dict, sort_dict_by_value
 # from .ETL_framework import do_ETL
--- ihutilities/io_utils.py
+++ ihutilities/io_utils.py
@@ -3,7 +3,11 @@
 from __future__ import unicode_literals
 
 import csv
+import operator
 import os
+import math
+
+from collections import OrderedDict
 
 def write_dictionary(filename, data):    
     keys = data[0].keys()
@@ -15,3 +19,29 @@ def write_dictionary(filename, data):
         if newfile:
             dict_writer.writeheader()
         dict_writer.writerows(data)
+
+def pretty_print_dict(dictionary):
+    # print("Feature names: {}\n".format(feature_names))
+    WIDTH = 160
+    # find longest feature_name
+    max_width = max([len(key) for key in dictionary.keys()]) + 2
+    # find out how many of longest feature name fit in 80 characters
+    n_columns = math.floor(WIDTH/(max_width + 7))
+    # Build format string
+    fmt = "%{}s:%3d".format(max_width)
+    # feed feature_names into format string
+    report = ''
+    i = 1
+    for key, value in dictionary.items():
+        report = report + fmt % (key,value)
+        if (i % n_columns) == 0:
+            report = report + "\n"
+        i = i + 1
+
+    print(report)
+    return report
+
+def sort_dict_by_value(unordered_dict):
+    sorted_dict = sorted(unordered_dict.items(), key=operator.itemgetter(1))
+    return OrderedDict(sorted_dict)
+
