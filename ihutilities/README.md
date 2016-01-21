3425c53dcb8125dbf3e8276136ebb00a9a5d92aa
Ian Hopkinson <ianhopkinson@googlemail.com>
2016-01-21 14:11:17 +0000

    Miscellaneous small fixes

--- ihutilities/README.md
+++ ihutilities/README.md
@@ -6,3 +6,4 @@
 * Extend testing to io_utils
 * Get ETL_framework to accept a function as a data source
 * Add in some ngram functionality
+* report_input_length in ETL_framework can be really slow for a large file
--- ihutilities/__init__.py
+++ ihutilities/__init__.py
@@ -2,7 +2,8 @@
 # encoding: utf-8
 
 from .db_utils import (configure_db, write_to_db, update_to_db, 
-                       drop_db_tables, read_db, finalise_db)
+                       drop_db_tables, read_db, finalise_db,
+                       db_config_template)
 from .git_utils import git_uncommitted_changes, git_sha
 from .io_utils import write_dictionary, pretty_print_dict, sort_dict_by_value
 # from .ETL_framework import do_ETL
