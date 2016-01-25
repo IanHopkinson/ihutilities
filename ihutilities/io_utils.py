bab2287be729a1b609b83b64d7fc3144bb8f3d84
Ian Hopkinson <ianhopkinson@googlemail.com>
2016-01-25 13:49:49 +0000

    Documented write_dictionary, and added an append flag

--- ihutilities/io_utils.py
+++ ihutilities/io_utils.py
@@ -3,16 +3,47 @@
 from __future__ import unicode_literals
 
 import csv
+import logging
 import operator
 import os
 import math
 
 from collections import OrderedDict
 
-def write_dictionary(filename, data):    
+def write_dictionary(filename, data, append=True):
+    """This function writes a list of dictionaries to a CSV file
+
+    Args:
+       filename (str): 
+            file path to the output file     
+       data (list of dictionaries):
+            A list of ordered dictionaries to write
+
+    Kwargs:
+       append (bool): 
+            if True then data is appended to an existing file 
+            if False and the file exists then the file is deleted 
+       
+    Returns:
+       No return value
+
+    Raises:
+
+    Usage:
+        >>> 
+    """    
     keys = data[0].keys()
 
     newfile =  not os.path.isfile(filename)
+
+    if not append and not newfile:
+        logging.warning("Append is False, and {} exists therefore file is being deleted".format(filename))
+        os.remove(filename)
+        newfile = True
+    elif not newfile and append:
+        logging.info("Append is True, and {} exists therefore data is being appended".format(filename))
+    else:
+        logging.info("New file {} is being created".format(filename))
 
     with open(filename, 'a') as output_file:
         dict_writer = csv.DictWriter(output_file, keys, lineterminator='\n')
