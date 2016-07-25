7a7f505cbeb355e61d889352320faf9f36b2b77d
Ian Hopkinson <ian.hopkinson@gbgplc.com>
2016-07-25 14:52:58 +0100

    Export the check_mysql_database_exists function

--- ihutilities/__init__.py
+++ ihutilities/__init__.py
@@ -5,7 +5,7 @@ This module contains utility functions relating to databases, git and I/O
 """
 from .db_utils import (configure_db, write_to_db, update_to_db, 
                        drop_db_tables, read_db, finalise_db,
-                       db_config_template)
+                       db_config_template, check_mysql_database_exists)
 from .git_utils import git_uncommitted_changes, git_sha
 from .io_utils import write_dictionary, pretty_print_dict, sort_dict_by_value
 from .shapefile_utils import (load_shapefile_data, summarise_shapefile, plot_shapefile,
