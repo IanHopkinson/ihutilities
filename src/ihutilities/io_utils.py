cde2e5451f07b86fc2538445d239089fcd1999b4
Ian Hopkinson <ian.hopkinson@gbgplc.com>
2023-03-21 09:39:00 +0000

    Move over all the datamesh_helpers io_utils and their tests
    (write_dictionary should be the only overwrite)

--- src/ihutilities/io_utils.py
+++ src/ihutilities/io_utils.py
@@ -3,71 +3,254 @@
 
 from __future__ import unicode_literals
 
+
+import codecs
 import csv
+import dataclasses
 import fnmatch
 import hashlib
 import io
 import logging
-import math
-import operator
 import os
 import shutil
 import time
 import zipfile
-from collections import OrderedDict
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, TextIO, Generator
+from pathlib import Path
 
 import requests
+import glob
+
+import boto3
+from botocore.client import Config
 
 logger = logging.getLogger(__name__)
 
+config = Config(connect_timeout=900, read_timeout=900, retries={"max_attempts": 3})
 
-def write_dictionary(
-    filename: str,
-    data: List[Dict[str, Any]],
-    append: Optional[bool] = True,
-    delimiter: Optional[str] = ",",
-    quoting: Optional[int] = csv.QUOTE_MINIMAL,
-    encoding: Optional[str] = "utf-8",
-) -> None:
-    """This function writes a list of dictionaries to a CSV file
 
-    Args:
-       filename (str):
-            file path to the output file
-       data (list of dictionaries):
-            A list of ordered dictionaries to write
-
-    Keyword args:
-       append (bool):
-            if True then data is appended to an existing file
-            if False and the file exists then the file is deleted
-       delimiter (str):
-            Delimiter character as per dictwriter interface
-    Returns:
-       No return value
-
-    Example:
-        >>>
-    """
-    keys = data[0].keys()
-
-    newfile = not os.path.isfile(filename)
-
-    if not append and not newfile:
-        logging.warning(
-            "Append is False, and {} exists therefore file is being deleted".format(filename)
-        )
-        os.remove(filename)
-        newfile = True
-    elif not newfile and append:
-        logger.info(
-            "Append is True, and {} exists therefore data is being appended".format(filename)
+def create_s3_client(
+    profile_name: Optional[str] = None,
+    aws_access_key_id: Optional[str] = None,
+    aws_secret_access_key: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> boto3.session.Session.client:
+    if os.getenv("AWS_PROFILE") not in ["", None] and profile_name is None:
+        profile_name = os.getenv("AWS_PROFILE")
+    if profile_name is not None:
+        session = boto3.Session(profile_name=profile_name)
+    elif s3_session is not None:
+        session = s3_session
+    elif aws_access_key_id is not None and aws_secret_access_key is not None:
+        session = boto3.Session(
+            aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key
         )
     else:
-        logging.info("New file {} is being created".format(filename))
+        session = boto3.Session()
 
-    with open(filename, "a", encoding=encoding, errors="ignore") as output_file:
+    client = session.client("s3", config=config)
+    return client
+
+
+def is_s3(file_uri: str) -> bool:
+    is_s3_uri = False
+    if file_uri.lower().startswith("s3://"):
+        is_s3_uri = True
+    return is_s3_uri
+
+
+def join_paths_local_or_s3(*path_parts: str) -> str:
+    full_path = ""
+    if is_s3(path_parts[0]):
+        full_path = "/".join(path_parts[1:])
+        full_path = full_path.replace("//", "/")
+        if path_parts[0].endswith("/"):
+            full_path = path_parts[0] + full_path
+        else:
+            full_path = path_parts[0] + "/" + full_path
+    else:
+        full_path = os.path.join(*path_parts)
+
+    return full_path
+
+
+def is_file_local_or_s3(file_uri: str):
+    is_file = False
+    if is_s3(file_uri):
+        matching_files = list_files_local_or_s3(file_uri)
+        if len(matching_files) == 1:
+            is_file = True
+    else:
+        is_file = os.path.isfile(file_uri)
+
+    return is_file
+
+
+def split_uri(full_uri: str) -> Tuple[str, str, str]:
+    bucket = ""
+    key = ""
+    filename = ""
+    if is_s3(full_uri):
+        full_uri = full_uri.replace("s3://", "")
+        parts = full_uri.split("/")
+        bucket = parts[0]
+        if len(parts) >= 2:
+            filename = parts[-1]
+            key = "/".join(parts[1:])
+        else:
+            filename = ""
+            key = ""
+        bucket = "s3://" + bucket
+    else:
+        bucket = str(Path(full_uri).parents[0])
+        key = ""
+        filename = Path(full_uri).name
+
+    return bucket, key, filename
+
+
+def write_to_file(
+    file_uri: str,
+    content: str,
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> str:
+
+    status = ""
+
+    if is_s3(file_uri):
+        s3_bucket, key, _ = split_uri(file_uri)
+        status = _write_file_to_s3(
+            key,
+            s3_bucket,
+            content,
+            aws_profile=aws_profile,
+            s3_session=s3_session,
+        )
+    else:
+        status = _write_file_to_local(
+            file_uri,
+            content,
+        )
+
+    return status
+
+
+def _write_file_to_local(
+    filepath: str,
+    content: str,
+    encoding: str = "utf-8",
+) -> str:
+
+    status = ""
+
+    if os.path.isfile(filepath):
+        status = f"Deleting existing existing {filepath}, before creating new file"
+        os.remove(filepath)
+    else:
+        status = f"Creating new file: {filepath}"
+
+    with open(filepath, "w", encoding=encoding) as documentation_file:
+        documentation_file.write(content)
+
+    return status
+
+
+def _write_file_to_s3(
+    key: str,
+    s3_bucket: str,
+    content: str,
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> str:
+
+    s3_bucket = s3_bucket.replace("s3://", "")
+    s3_client = create_s3_client(profile_name=aws_profile, s3_session=s3_session)
+    objs = list(s3_client.list_objects(Bucket=s3_bucket, Prefix=key))
+    filepath = f"s3://{s3_bucket}/{key}"
+    if "Contents" in objs:
+        status = f"Deleting existing existing {filepath}, before creating new file"
+        s3_client.delete_object(Bucket=s3_bucket, Key=key)
+    else:
+        status = f"Creating new file: {filepath}"
+
+    s3_client.put_object(Bucket=s3_bucket, Key=key, Body=content)
+
+    return status
+
+
+def write_dictionary(
+    file_uri: str,
+    data: List[Dict[str, Any]],
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+    append: bool = True,
+    delimiter: str = ",",
+    quoting: int = csv.QUOTE_MINIMAL,
+    encoding: str = "utf-8",
+) -> str:
+
+    status = ""
+    if len(data) == 0:
+        raise IndexError("write_dictionary was supplied with an empty list")
+
+    if dataclasses.is_dataclass(data[0]):
+        temp_data = []
+        for row in data:
+            temp_data.append(dataclasses.asdict(row))
+        data = temp_data
+    try:
+        _ = data[0].keys()
+    except AttributeError as attribute_exception:
+        raise AttributeError(
+            "write_dictionary was not supplied with a list of dictionaries"
+        ) from attribute_exception
+
+    if is_s3(file_uri):
+        s3_bucket, key, _ = split_uri(file_uri)
+        status = _write_dictionary_to_s3(
+            key,
+            s3_bucket,
+            data,
+            aws_profile=aws_profile,
+            s3_session=s3_session,
+            append=append,
+            delimiter=delimiter,
+            quoting=quoting,
+        )
+    else:
+        status = _write_dictionary_to_local_file(
+            file_uri,
+            data,
+            append=append,
+            delimiter=delimiter,
+            quoting=quoting,
+            encoding=encoding,
+        )
+
+    return status
+
+
+def _write_dictionary_to_local_file(
+    filepath: str,
+    data: List[Dict[str, Any]],
+    append: bool = True,
+    delimiter: str = ",",
+    quoting: int = csv.QUOTE_MINIMAL,
+    encoding: str = "utf-8",
+) -> str:
+
+    status = ""
+    keys = data[0].keys()
+
+    newfile = not os.path.isfile(filepath)
+
+    status = _make_write_dictionary_status(append, filepath, newfile)
+    if not append and not newfile:
+        os.remove(filepath)
+        newfile = True
+
+    with open(filepath, "a", encoding=encoding, errors="ignore") as output_file:
         dict_writer = csv.DictWriter(
             output_file,
             keys,
@@ -79,6 +262,65 @@ def write_dictionary(
         if newfile:
             dict_writer.writeheader()
         dict_writer.writerows(data)
+
+    return status
+
+
+def _write_dictionary_to_s3(
+    key: str,
+    s3_bucket: str,
+    data: List[Dict[str, Any]],
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+    append: bool = True,
+    delimiter: str = ",",
+    quoting: int = csv.QUOTE_MINIMAL,
+) -> str:
+
+    s3_bucket = s3_bucket.replace("s3://", "")
+    s3_client = create_s3_client(profile_name=aws_profile, s3_session=s3_session)
+    objs = list(s3_client.list_objects(Bucket=s3_bucket, Prefix=key))
+    if "Contents" in objs:
+        newfile = False
+    else:
+        newfile = True
+
+    filepath = f"s3://{s3_bucket}/{key}"
+    status = _make_write_dictionary_status(append, filepath, newfile)
+    if not append and not newfile:
+        s3_client.delete_object(Bucket=s3_bucket, Key=key)
+        newfile = True
+
+    stream = io.StringIO()
+    headers = list(data[0].keys())
+    writer = csv.DictWriter(
+        stream, fieldnames=headers, lineterminator="\n", delimiter=delimiter, quoting=quoting
+    )
+    output_data = None
+    if newfile:
+        writer.writeheader()
+        writer.writerows(data)
+        output_data = stream.getvalue()
+    else:
+        old_data = s3_client.get_object(Bucket=s3_bucket, Key=key)["Body"].read().decode("utf-8")
+        writer.writerows(data)
+        new_data = stream.getvalue()
+        output_data = old_data + new_data
+
+    s3_client.put_object(Bucket=s3_bucket, Key=key, Body=output_data)
+
+    return status
+
+
+def _make_write_dictionary_status(append: bool, filepath: str, newfile: bool) -> str:
+    status = ""
+    if not append and not newfile:
+        status = f"Append is False, and {filepath} exists therefore file is being deleted"
+    elif not newfile and append:
+        status = f"Append is True, and {filepath} exists therefore data is being appended"
+    else:
+        status = f"New file {filepath} is being created"
+    return status
 
 
 def calculate_file_sha(filepath: str, encoding="utf-8-sig"):
@@ -266,3 +508,108 @@ def download_file_from_url(url: str, local_filepath: str) -> str:
     logger.info("Download took {:.2f}seconds for {:.2f}mb\n".format(t1 - t0, chunk_count / 1024))
 
     return local_filepath
+
+
+def iterator_from_filepath(
+    file_path: str,
+    delimiter: str = ",",
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> Generator[Dict, None, None]:
+    if is_s3(file_path):
+        bucket_name, key, _ = split_uri(file_path)
+        bucket_name = bucket_name.replace("s3://", "")
+        s3_client = create_s3_client(profile_name=aws_profile, s3_session=s3_session)
+        try:
+            file_handle = s3_client.get_object(Bucket=bucket_name, Key=key)
+        except s3_client.exceptions.NoSuchKey:
+            raise FileNotFoundError(f"{bucket_name}/{key} not found")
+
+        for row in csv.DictReader(
+            codecs.getreader("utf-8")(file_handle["Body"]), delimiter=delimiter
+        ):
+            yield row
+    else:
+        with open(os.path.join(file_path), encoding="utf-8-sig") as file_handle:
+            reader = csv.DictReader(file_handle, delimiter=delimiter)
+            for row in reader:
+                yield row
+
+
+def file_handle_from_filepath(
+    file_path: str,
+    as_bytes: bool = False,
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> TextIO:
+    file_handle = None
+    if is_s3(file_path):
+        bucket_name, key, file_ = split_uri(file_path)
+        bucket_name = bucket_name.replace("s3://", "")
+        s3_client = create_s3_client(profile_name=aws_profile, s3_session=s3_session)
+        try:
+            s3_object = s3_client.get_object(Bucket=bucket_name, Key=key)
+        except s3_client.exceptions.NoSuchKey:
+            raise FileNotFoundError(f"Bucket name: {bucket_name} with key: {key} not found")
+
+        file_handle = codecs.getreader("utf-8")(s3_object["Body"])
+    else:
+        if as_bytes:
+            file_handle = open(os.path.join(file_path), "rb")
+        else:
+            file_handle = open(os.path.join(file_path), encoding="utf-8-sig")
+
+    return file_handle
+
+
+def list_files_local_or_s3(
+    file_pattern: str,
+    aws_profile: Optional[str] = None,
+    s3_session: Optional[boto3.session.Session] = None,
+) -> List[str]:
+    file_list = []
+    if is_s3(file_pattern):
+        s3_bucket, _, _ = split_uri(file_pattern)
+        bucket_name = s3_bucket.replace("s3://", "")
+        pattern = file_pattern.replace("s3://" + bucket_name + "/", "")
+        subdirectory_search = False
+        if pattern.endswith("/"):
+            subdirectory_search = True
+            pattern += "*"
+
+        s3_client = create_s3_client(profile_name=aws_profile, s3_session=s3_session)
+        list_object_response = s3_client.list_objects_v2(Bucket=bucket_name)
+        if list_object_response["IsTruncated"]:
+            print(
+                f"Warning: list_objects_local_or_s3 returned truncated results for {s3_bucket}",
+                flush=True,
+            )
+        for object_key in list_object_response["Contents"]:
+
+            if fnmatch.fnmatch(object_key["Key"], pattern):
+                if subdirectory_search:
+                    full_path = join_paths_local_or_s3(s3_bucket, object_key["Key"].split("/")[0])
+                else:
+                    full_path = join_paths_local_or_s3(s3_bucket, object_key["Key"])
+                file_list.append(full_path)
+    else:
+        file_list = glob.glob(file_pattern)
+
+    return file_list
+
+
+def expand_file_path(examplefile: Optional[str]) -> Optional[str]:
+    if examplefile is None:
+        return None
+    if not is_s3(examplefile):
+        if examplefile == "":
+            return os.getcwd()
+
+        if not Path(examplefile).is_absolute():
+            example_path_full = os.path.join(os.getcwd(), examplefile)
+        else:
+            example_path_full = examplefile
+    else:
+        example_path_full = examplefile
+
+    return example_path_full
--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -4,7 +4,37 @@
 import os
 import unittest
 
-from ihutilities import git_calculate_file_sha, calculate_file_sha, colour_text
+import os
+import pytest
+import sys
+
+from pathlib import Path
+from unittest import TestCase
+
+from moto import mock_s3
+from ihutilities.io_utils import (
+    create_s3_client,
+    iterator_from_filepath,
+    file_handle_from_filepath,
+    split_uri,
+    join_paths_local_or_s3,
+    write_dictionary,
+    list_files_local_or_s3,
+    expand_file_path,
+)
+
+from ihutilities import git_calculate_file_sha, calculate_file_sha
+
+PP_REFERENCE_DATA_DIRECTORY = os.path.join(
+    Path(__file__).parents[0], "fixtures", "land-registry-price-paid"
+)
+
+TEMP_FILE_PATH = os.path.join(Path(__file__).parents[0], "temp", "tmp.csv")
+DICT_LIST = [
+    {"a": 1, "b": 2, "c": 3},
+    {"a": 4, "b": 5, "c": 6},
+    {"a": 7, "b": 8, "c": 9},
+]
 
 
 @unittest.skip("Comparison between my implementation of sha and git sha has never worked")
@@ -31,27 +61,196 @@ class ShaCalculationTests(unittest.TestCase):
         self.assertEqual(git_calculate_file_sha(norm_path), calculate_file_sha(norm_path))
 
 
-class ColourTextTests(unittest.TestCase):
-    def test_the_text_colours_are_correct(self):
-        colours = [
-            "red",
-            "green",
-            "blue",
-            "cyan",
-            "white",
-            "yellow",
-            "magenta",
-            "grey",
-            "black",
-            "not available",
-        ]
-        print("\n", flush=True)
-        for colour in colours:
-            print(
-                "{}: {} light: {}".format(
-                    colour,
-                    colour_text(colour, colour=colour),
-                    colour_text(colour, colour="light_" + colour),
-                ),
-                flush=True,
-            )
+def test_write_dictionary_raises_an_index_error():
+    TestCase().assertRaises(IndexError, write_dictionary, TEMP_FILE_PATH, [])
+
+
+def test_write_dictionary_raises_a_key_error():
+    TestCase().assertRaises(AttributeError, write_dictionary, TEMP_FILE_PATH, [0, 1, 2])
+
+
+def test_write_dictionary_to_local_file():
+
+    if os.path.isfile(TEMP_FILE_PATH):
+        os.remove(TEMP_FILE_PATH)
+
+    status = write_dictionary(TEMP_FILE_PATH, DICT_LIST)
+    rows_read = list(iterator_from_filepath(TEMP_FILE_PATH))
+
+    TestCase().assertEqual(len(rows_read), 3)
+    TestCase().assertDictEqual(rows_read[0], {"a": "1", "b": "2", "c": "3"})
+    TestCase().assertIn("New file", status)
+    TestCase().assertIn("is being created", status)
+
+
+@mock_s3
+def test_write_dictionary_to_s3():
+    filename = "tmp.csv"
+    s3_uri = f"s3://test-bucket/{filename}"
+    s3_bucket, _, _ = split_uri(s3_uri)
+    s3_client = create_s3_client()
+
+    bucket_name = s3_bucket.replace("s3://", "")
+    s3_client.create_bucket(
+        Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": "eu-west-2"}
+    )
+
+    objs = list(s3_client.list_objects(Bucket=bucket_name, Prefix=filename))
+    if "Contents" in objs:
+        s3_client.delete_object(Bucket=bucket_name, Key=filename)
+
+    status = write_dictionary(s3_uri, DICT_LIST)
+    rows_read = list(iterator_from_filepath(s3_uri))
+
+    TestCase().assertEqual(len(rows_read), 3)
+    TestCase().assertDictEqual(rows_read[0], {"a": "1", "b": "2", "c": "3"})
+    TestCase().assertEqual(status, "New file s3://test-bucket/tmp.csv is being created")
+
+
+@pytest.mark.skipif(
+    not sys.platform.startswith("win"),
+    reason="Skipping local file directory split for Azure Devops since Devops is on Ubuntu",
+)
+def test_split_uri_local_file():
+    local_uri = "C:\\temp\\folder\\filename.csv"
+    directory, key, filename = split_uri(local_uri)
+
+    TestCase().assertEqual(directory, "C:\\temp\\folder")
+    TestCase().assertEqual(key, "")
+    TestCase().assertEqual(filename, "filename.csv")
+
+
+def test_split_uri_s3():
+    s3_uri = "s3://gbgdmsp-staging-land-registry-house-price-index/2021-03-01/Indices-2021-03.csv"
+    bucket, key, filename = split_uri(s3_uri)
+
+    TestCase().assertEqual(bucket, "s3://gbgdmsp-staging-land-registry-house-price-index")
+    TestCase().assertEqual(key, "2021-03-01/Indices-2021-03.csv")
+    TestCase().assertEqual(filename, "Indices-2021-03.csv")
+
+
+def test_split_uri_works_on_a_bucket_only_s3_uri():
+    local_uri = "s3://gbgdmsp-staging-land-registry-house-price-index"
+    bucket, key, filename = split_uri(local_uri)
+
+    TestCase().assertEqual(bucket, "s3://gbgdmsp-staging-land-registry-house-price-index")
+    TestCase().assertEqual(key, "")
+    TestCase().assertEqual(filename, "")
+
+
+@mock_s3
+def test_file_handle_from_filepath_raises_file_not_found_on_s3(make_s3_test_files):
+    s3_file_uri = "s3://gbgdmsp-staging-land-registry-price-paid/wrong_file.md"
+    bucket_name = "gbgdmsp-staging-land-registry-price-paid"
+    file_list = [
+        (
+            "documentation.md",
+            os.path.join(PP_REFERENCE_DATA_DIRECTORY, "documentation.md"),
+        )
+    ]
+
+    make_s3_test_files(
+        bucket_name,
+        file_list,
+    )
+
+    TestCase().assertRaisesRegex(
+        FileNotFoundError,
+        "Bucket name: gbgdmsp-staging-land-registry-price-paid with key: wrong_file.md not found",
+        file_handle_from_filepath,
+        s3_file_uri,
+    )
+
+
+def test_list_files_local():
+    file_directory = os.path.join(Path(__file__).parents[0])
+
+    files = list_files_local_or_s3(f"{file_directory}/*.py")
+
+    TestCase().assertEqual(len(files), 13)
+
+
+@mock_s3
+def test_list_files_s3_with_pattern(make_s3_test_files):
+    s3_pattern = "s3://gbgdmsp-staging-land-registry-price-paid/*/documentation.md"
+    bucket_name = "gbgdmsp-staging-land-registry-price-paid"
+    source_file = os.path.join(PP_REFERENCE_DATA_DIRECTORY, "documentation.md")
+    file_list = [
+        (
+            "2022-09-22/documentation.md",
+            source_file,
+        ),
+        (
+            "2022-08-22/documentation.md",
+            source_file,
+        ),
+        (
+            "2022-07-22/documentation.md",
+            source_file,
+        ),
+    ]
+
+    make_s3_test_files(
+        bucket_name,
+        file_list,
+    )
+
+    files = list_files_local_or_s3(s3_pattern)
+
+    TestCase().assertEqual(len(files), 3)
+
+
+@mock_s3
+def test_list_files_s3_for_subdirectories(make_s3_test_files):
+    s3_pattern = "s3://gbgdmsp-staging-land-registry-price-paid/*/"  # trailing / for directory
+    bucket_name = "gbgdmsp-staging-land-registry-price-paid"
+    source_file = os.path.join(PP_REFERENCE_DATA_DIRECTORY, "documentation.md")
+    subdirectories = ["2022-07-22", "2022-08-22", "2022-09-22"]
+    file_list = [(x + "/documentation.md", source_file) for x in subdirectories]
+
+    make_s3_test_files(
+        bucket_name,
+        file_list,
+    )
+
+    files = list_files_local_or_s3(s3_pattern)
+
+    TestCase().assertEqual(
+        files,
+        [
+            "s3://gbgdmsp-staging-land-registry-price-paid/2022-07-22",
+            "s3://gbgdmsp-staging-land-registry-price-paid/2022-08-22",
+            "s3://gbgdmsp-staging-land-registry-price-paid/2022-09-22",
+        ],
+    )
+
+
+def test_join_paths_local_or_s3():
+    measured_s3_path = join_paths_local_or_s3(
+        "s3://some-bucket-name", "2020-09-05", "attributes.csv"
+    )
+    expected_s3_path = "s3://some-bucket-name/2020-09-05/attributes.csv"
+    TestCase().assertEqual(measured_s3_path, expected_s3_path)
+
+    measured_s3_path_2 = join_paths_local_or_s3("s3://some-bucket-name", "*/")
+    expected_s3_path_2 = "s3://some-bucket-name/*/"
+    TestCase().assertEqual(measured_s3_path_2, expected_s3_path_2)
+
+    measured_s3_path = join_paths_local_or_s3(
+        "s3://some-bucket-name", "2020-09-05/", "attributes.csv"
+    )
+    expected_s3_path = "s3://some-bucket-name/2020-09-05/attributes.csv"
+    TestCase().assertEqual(measured_s3_path, expected_s3_path)
+
+
+def test_expand_file_path_makes_a_path_absolute():
+    pattern = "tests/fixtures/*.csv"
+    example_path_full = expand_file_path(pattern)
+    TestCase().assertEqual(example_path_full, os.path.join(os.getcwd(), pattern))
+
+
+def test_expand_file_path_expands_a_list_of_files():
+    pattern = "tests/fixtures/land-registry-price-paid/*.md"
+    full_pattern = expand_file_path(pattern)
+    example_files = list_files_local_or_s3(full_pattern)
+    TestCase().assertEqual(len(example_files), 1)
